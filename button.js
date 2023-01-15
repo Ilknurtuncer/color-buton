1f2bbe217fb6dd4c196491b848fbf2c1cb22e12d
Ilknur Tuncer <ilknurtuncer67@gmail.com>
2023-01-15 17:51:53 +0300

    first commit

---
+++ button.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById("btn");
+const colorChange = document.getElementById("colorChange");
+
+console.log( document.getElementById("btn"));
+
+// btn.style.backgroundColor = 'yellow'
+
+btn.onmouseover = function (){
+    btn.style.backgroundColor = changeColor
+}
